9e129e43afd03ac0ca61d69340af7c10b7d2394c
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-09-06 21:17:37 -0600

    xxxx

--- routes/route_presupuesto.py
+++ routes/route_presupuesto.py
@@ -9,7 +9,7 @@ route_presupuesto = APIRouter()
 @route_presupuesto.get("/tb_presupuesto")
 def get_presupuesto(filtro=''):
    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as dict_cur:
-      dict_cur.execute("select * from public.tb_presupuesto where proyecto like '%"+ filtro.upper() +"%' order by fecha_cambio desc limit 10")
+      dict_cur.execute("select presupuesto,proyecto from public.tb_presupuesto where proyecto like '%"+ filtro.upper() +"%' order by fecha_cambio desc limit 10")
       result = dict_cur.fetchall()
    return result
 
